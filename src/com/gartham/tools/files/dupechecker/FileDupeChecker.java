1605959c976d8924ea872b37624c4d322301e83c
Gartham <gartham@dusttoash.org>
2022-09-29 12:11:44 -0500

    Update FileDupeChecker.java

--- src/com/gartham/tools/files/dupechecker/FileDupeChecker.java
+++ src/com/gartham/tools/files/dupechecker/FileDupeChecker.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Stack;
 
 import org.alixia.javalibrary.strings.StringTools;
@@ -42,10 +45,15 @@ public class FileDupeChecker {
 				return false;
 			return true;
 		}
+		
+		@Override
+		public String toString() {
+			return StringTools.toHexString(bytes);
+		}
 
 	}
 
-	private static final int BUFFER_SIZE = 65535, STATUS_DELAY_GAP = 1000;
+	private static final int BUFFER_SIZE = 65535, STATUS_DELAY_GAP = 10000;
 	private static final boolean PRINT_STATUS = true;
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
@@ -56,7 +64,7 @@ public class FileDupeChecker {
 		else if (!file.isDirectory())
 			System.err.println("You need to specify a directory to search through!");
 		else {
-			Map<Hash, File> hashtable = new HashMap<>();
+			Map<Hash, List<File>> hashtable = new HashMap<>();
 			MessageDigest hasher = MessageDigest.getInstance("SHA-256");
 
 			Stack<File> dirchildren = new Stack<>();
@@ -103,15 +111,25 @@ public class FileDupeChecker {
 							byte[] hash = hasher.digest();
 							Hash h = new Hash(hash);
 							if (hashtable.containsKey(h))
-								System.out.println("Duplicate between files: \n\t" + hashtable.get(h) + "\n\t" + f);
-							else
-								hashtable.put(h, f);
+								hashtable.get(h).add(f);
+							else {
+								List<File> files = new ArrayList<>(1);
+								hashtable.put(h, files);
+								files.add(f);
+							}
 						} catch (IOException e) {
 							System.err.println("An error occurred with OPENING the file: " + f + ".\n\tError message: "
 									+ e.getMessage());
 						}
 				}
 			}
+
+			for (Entry<Hash, List<File>> e : hashtable.entrySet())
+				if (e.getValue().size() != 1) {
+					System.out.println("Files with hash " + e.getKey() + ':');
+					for (File f : e.getValue())
+						System.out.println("\t" + f);
+				}
 
 		}
 	}
